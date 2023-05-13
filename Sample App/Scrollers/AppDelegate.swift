ae0b76ce6ed01a1f493eedb408e6ab5a6c39b509
John Brayton <jbrayton@goldenhillsoftware.com>
2023-05-13 19:09:12 -0400

    Added custom scroller view, but it is just a copy of Starting Point for now. Made the 3 windows cascade.

--- Sample App/Scrollers/AppDelegate.swift
+++ Sample App/Scrollers/AppDelegate.swift
@@ -12,16 +12,24 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     var startingPointWindowController: NSWindowController!
     var cssScrollerWindowController: NSWindowController!
+    var customScrollerWindowController: NSWindowController!
 
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         
+        var cascadePoint = NSPoint.zero
+
         self.startingPointWindowController = (NSStoryboard(name: "StartingPointWindow", bundle: nil).instantiateController(withIdentifier: "StartingPointWindow") as! NSWindowController)
         self.startingPointWindowController.showWindow(nil)
+        cascadePoint = self.startingPointWindowController.window!.cascadeTopLeft(from: cascadePoint)
 
         self.cssScrollerWindowController = (NSStoryboard(name: "CSSScrollerWindow", bundle: nil).instantiateController(withIdentifier: "CSSScrollerWindow") as! NSWindowController)
         self.cssScrollerWindowController.showWindow(nil)
+        cascadePoint = self.cssScrollerWindowController.window!.cascadeTopLeft(from: cascadePoint)
 
+        self.customScrollerWindowController = (NSStoryboard(name: "CustomScrollerWindow", bundle: nil).instantiateController(withIdentifier: "CustomScrollerWindow") as! NSWindowController)
+        self.customScrollerWindowController.showWindow(nil)
+        cascadePoint = self.customScrollerWindowController.window!.cascadeTopLeft(from: cascadePoint)
     }
 
 }
---
+++ Sample App/Scrollers/Custom Scroller/CustomScrollerViewController.swift
@@ -0,0 +1,33 @@
+//
+//  CustomScrollerViewController.swift
+//  Scrollers
+//
+//  Created by John Brayton on 5/13/23.
+//
+
+import AppKit
+import WebKit
+
+class CustomScrollerViewController : NSViewController {
+    
+    var yellowBackgroundView: NSView!
+    var webView: WKWebView!
+    
+    override func viewDidLoad() {
+        
+        // This view has the same background color as the webview body.
+        // This appears to have no visible effect, but helps demonstrate
+        // that putting solid background behind the scrollable webview does
+        // not help.
+        self.yellowBackgroundView = YellowBackgroundView(frame: self.view.bounds)
+        self.view.addSubview(yellowBackgroundView)
+
+        self.webView = WKWebView(frame: self.view.bounds)
+        self.view.addSubview(self.webView)
+        let fileUrl = Bundle.main.url(forResource: "customscroller", withExtension: "html")!
+        let data = try! Data(contentsOf: fileUrl)
+        let html = String(data: data, encoding: .utf8)!
+        self.webView.loadHTMLString(html, baseURL: nil)
+    }
+    
+}
